dfbfacfe5db426496546b255e8a576be88139eef
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2020-02-22 17:50:13 +0700

    Update esp_initial_program.ino

--- esp_initial_program/esp_initial_program.ino
+++ esp_initial_program/esp_initial_program.ino
@@ -1,160 +1,113 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
 #include <SPI.h>
 #include <MFRC522.h>
 
-const char* ssid = "iPhone";
-const char* password = "rajaterakhir";
+const int     RST_PIN         = D1;
+const int     SS_PIN          = D2;
 
-String server = "http://192.168.43.69:8000/api/attendances";
+const char*   WIFI_SSID       = "iPhone";
+const char*   WIFI_PASSWORD   = "rajapertama";
 
-String uid = "00 00 00 00";
-int classId = 1;
-String macAddress = "";
-String timestamps = "2020-04-19T00:30:07.000Z";
+const String  SERVER_HOST     = "192.168.43.69";
+const int     SERVER_PORT     = 8000;
 
-String jsonRequest;
+String        server          = "http://" + SERVER_HOST + ":" + SERVER_PORT + "/api/attendances";
 
-const char* name;
-const char* nim;
-bool access = false;
-bool isLecturer = false;
+String        uid             = "";
+String        mac_address     = "";
+
+String        json_request    = "";
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  
+  Serial.begin(9600);
   while (!Serial) return;
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting");
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println();
-  Serial.print("Connected to: ");
-  Serial.println(ssid);
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:  
-  macAddress = getMacAddress();
-  jsonRequest = jsonEncode(uid, classId, macAddress, timestamps);
+  
+  String response;
+  
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
-  String uid = getUID();
-  if (uid.substring(1) == "25 26 60 2B") //change here the UID of the card/cards that you want to give access
-  {
-    if (WiFi.status() == WL_CONNECTED) {
-      sendDataToServer(jsonRequest);
-    } else {
-      Serial.println("Error sending data");
-    }
-  } else {
-    Serial.println("Access denied");
-    Serial.println();
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
   }
   
-//  if (WiFi.status() == WL_CONNECTED) {
-//    sendDataToServer(jsonRequest);
-//  } else {
-//    Serial.println("Error sending data");
-//  }
-
-  delay(10000);
+  mac_address   = getMacAddress();
+  uid           = getUID();
+  
+  json_request  = json_encode(uid, mac_address);
+  response      = send_data_to_server(json_request);
+  send_data_to_arduino(response);
+  delay(3000);
 }
 
-void sendDataToServer(String data) {
+String send_data_to_server(String data) {
   HTTPClient http;
-  http.begin("http://192.168.43.69:8000/api/attendances");
+  
+  http.begin(server);
   http.addHeader("Content-Type", "application/json");
+  
   int httpCode = http.POST(data);
   String payload = http.getString();
-
-  Serial.print("HTTP Code: ");
-  Serial.println(httpCode);
-  Serial.print("Payload: ");
-  Serial.println(payload);
   
   http.end();
+
+  return payload;
 }
 
-String jsonEncode(String uid, int classId, String macAddress, String timestamps) {
-  String prettyJson;
-  const int capacity = JSON_OBJECT_SIZE(4) + 91;
+String json_encode(String uid, String macAddress) {
+  const int capacity    = JSON_OBJECT_SIZE(2) + 78;
+  
+  String pretty_json;
   DynamicJsonDocument json(capacity);
-  json["uid"] = uid;
-  json["class_id"] = classId;
-  json["mac_address"] = macAddress;
-  json["timestamps"] = timestamps;
-  serializeJsonPretty(json, prettyJson);
-  return prettyJson;
-}
-
-String jsonDecode(String data) {
-  const size_t capacity = JSON_OBJECT_SIZE(4) + 70;
-  StaticJsonDocument<capacity> json;
-  deserializeJson(json, data);
-
-  name = json["name"];
-  nim = json["nim"];
-  access = json["access"];
-  isLecturer = json["is_lecturer"];
+  
+  json["uid"]           = uid;
+  json["mac_address"]   = macAddress;
+  
+  serializeJsonPretty(json, pretty_json);
+  
+  return pretty_json;
 }
 
 String getMacAddress() {
   return WiFi.macAddress();
 }
 
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
-
-void handleRoot() {
-  Serial.println("Enter handleRoot");
-  String header;
-  if (!is_authentified()) {
-    String header = "HTTP/1.1 301 OK\r\nLocation: /login\r\nCache-Control: no-cache\r\n\r\n";
-    server.sendContent(header);
-    return;
-  }
-  String content = "<html><h2>WELCOME TO NODEMCU WEB SERVER!</font></h2><br>";
-  content += "<br><a href=\"/login?DISCONNECT=YES\"><b>LOGOUT</b></font></button></font></body></html>";
-  
-  if (server.hasHeader("User-Agent")){
-    content += "<p>The user agent used is : " + server.header("User-Agent") + "<br><br>";
-  }
-  server.send(200, "text/html", content);
-}
-
 String getUID() {
   String content = "";
-  byte letter;
-
-  Serial.print("UID: ");
+  
   for (int i = 0; i < mfrc522.uid.size; i++) {
-  //    readCard[i] = mfrc522.uid.uidByte[i];
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    content.concat(i != 3 ? ":" : "");
   }
-  content.toUpperCase();
-  Serial.println(content);
+  
+  content.toUpperCase();  
   mfrc522.PICC_HaltA();
   
   return content;
+}
+
+void send_data_to_arduino(String json) {
+   Serial.println(json);
 }
